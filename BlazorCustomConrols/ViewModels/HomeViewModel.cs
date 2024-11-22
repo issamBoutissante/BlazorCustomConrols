6903eb34b8a37fcdea6c69b209f8309e8a757b57
Issam Boutissante <boutissante.issam.dev@gmail.com>
2024-11-22 08:22:15 +0100

    Add project files.

---
+++ BlazorCustomConrols/ViewModels/HomeViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace BlazorCustomConrols.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        private readonly Dictionary<string, string> _values = new();
+
+        public string this[string key]
+        {
+            get => _values.ContainsKey(key) ? _values[key] : string.Empty;
+            set
+            {
+                if (key == "LastName")
+                {
+                    _values[key] = value;
+                    OnPropertyChanged(key);
+                    OnPropertyChanged(nameof(FullName)); // Update FullName when LastName changes
+                }
+                else
+                {
+                    _values[key] = value;
+                    OnPropertyChanged(key);
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get => this[nameof(FirstName)];
+            set
+            {
+                this[nameof(FirstName)] = value;
+                OnPropertyChanged(nameof(FullName)); // Update FullName when FirstName changes
+            }
+        }
+
+  
+        public string FullName => $"{FirstName} {this["LastName"]}";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
