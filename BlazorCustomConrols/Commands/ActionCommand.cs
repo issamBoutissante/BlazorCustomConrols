dd21cd1248fd3bd039ed06ada69d185b6d3272f8
Issam Boutissante <boutissante.issam.dev@gmail.com>
2024-12-03 17:47:29 +0100

    Added Commands and LookupControl

---
+++ BlazorCustomConrols/Commands/ActionCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace BlazorCustomConrols.Commands;
+
+public class ActionCommand : ICommand
+{
+    private Action action_;
+    private Func<bool>? canAct_;
+    public ActionCommand(Action ac) => action_ = ac;
+
+    public ActionCommand(Action ac, Func<bool>? canExecute)
+    {
+        action_ = ac;
+        canAct_ = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (action_ is null) return false;
+        return (canAct_ is null) ? true : canAct_.Invoke();
+    }
+
+    public void Execute(object? parameter) => action_?.Invoke();
+
+    public event EventHandler? CanExecuteChanged;
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, noArgs);
+
+    private static readonly EventArgs noArgs = new EventArgs();
+
+}
---
+++ BlazorCustomConrols/Commands/AsyncCommand.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Input;
+
+namespace BlazorCustomConrols.Commands;
+
+public interface IAsyncCommand : ICommand
+{
+    Task ExecuteAsync();
+    bool CanExecute();
+}
+
+
+//-------------------------------------------------------------------------------------------------!
+
+public static class TaskUtility
+{
+    public static async void FireAndForgetSafeAsync(this Task task)
+#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
+    {
+        try
+        {
+            await task;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
+//-------------------------------------------------------------------------------------------------!
+
+public class AsyncCommand : IAsyncCommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    private bool isExecuting_;
+    private readonly Func<Task> execute_;
+    private readonly Func<bool>? canExecute_;
+
+    public AsyncCommand(
+        Func<Task> execute,
+        Func<bool>? canExecute = null
+       )
+    {
+        execute_ = execute;
+        canExecute_ = canExecute;
+    }
+
+    public bool CanExecute()
+    {
+        return !isExecuting_ && (canExecute_?.Invoke() ?? true);
+    }
+
+    public async Task ExecuteAsync()
+    {
+        if (CanExecute())
+        {
+            try
+            {
+                isExecuting_ = true;
+                await execute_();
+            }
+            finally
+            {
+                isExecuting_ = false;
+            }
+        }
+
+        RaiseCanExecuteChanged();
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    #region Explicit implementations
+    bool ICommand.CanExecute(object? parameter) => CanExecute();
+
+
+    void ICommand.Execute(object? parameter) => ExecuteAsync().FireAndForgetSafeAsync();
+
+    #endregion
+}
