aa90f105760a66abed502e1fe2f395e087c79243
Issam Boutissante <boutissante.issam.dev@gmail.com>
2024-12-04 12:51:24 +0100

    added IbaseEntity

---
+++ BlazorCustomConrols/Models/IBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorCustomConrols.Models
+{
+    public interface IBaseEntity
+    {
+        ulong BarID { get; set; }
+        string Title { get; set; }
+        string Name { get; set; }
+        string this[string propertyName] { get; }
+    }
+}
---
+++ BlazorCustomConrols/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorCustomConrols.Models
+{
+    public class Job : IBaseEntity
+    {
+        public ulong BarID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        public string this[string propertyName] =>
+            propertyName switch
+            {
+                nameof(BarID) => BarID.ToString(),
+                nameof(Title) => Title,
+                nameof(Name) => Name,
+                _ => throw new ArgumentException($"Invalid property: {propertyName}")
+            };
+    }
+}
